278dec913277bf58cb9bb69c2979617c7f8146ad
Fabrice Chabance <fabrice.chabance@gmail.com>
2023-03-21 20:43:46 +0900

    KMKS-1 adding web controller

---
+++ scripts/create_web_controller.sh
@@ -0,0 +1,4 @@
+kubectl create deployment web --image=gcr.io/google-samples/hello-app:1.0
+kubectl expose deployment web --type=NodePort --port=8080
+kubectl create deployment web2 --image=gcr.io/google-samples/hello-app:2.0
+kubectl expose deployment web2 --port=8080 --type=NodePort
